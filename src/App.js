00abf7f6fb300d0e67287ad639239cd6fb5a08f4
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2021-07-15 16:01:46 +0530

    Day 2 Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Image from "./Image";
+
+const Heading = ({ name, school, city }) => {
+  // console.log(name);
+  console.log(name, school, city);
+  return (
+    <h1>{` HELLO ${name} ${school} ${city}
+     `}</h1>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Heading name="Prakhar" school="Amity" city="Patna" />
+      <Heading name="Nupur" school="BITS" city="Bihta" />
+      <p>Hi</p>
+      {/* <img
+        src="https://images.unsplash.com/photo-1530636041143-c07787be24e2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1653&q=80"
+        alt=""
+      /> */}
+      {/* <img src={X} alt="" />
+      <img className="image-2" src={X} alt="" /> */}
+      <Image />
     </div>
   );
 }
---
+++ src/Image.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ImageForRoom from "./michal-vrba-vElTvnQVNA4-unsplash.jpeg";
+const Image = () => {
+  return (
+    <div>
+      <img src={ImageForRoom} alt="" />
+    </div>
+  );
+};
+
+export default Image;
